475f5d7c9fbd430b804a1ca26a230c46d4dddfd5
ajay kondru <ajay.kondru@gmail.com>
2024-08-13 19:15:07 +0530

    CORS fix

--- src/main/java/com/org/mybookstore/api/book/BookController.java
+++ src/main/java/com/org/mybookstore/api/book/BookController.java
@@ -3,6 +3,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/books")
+@CrossOrigin(origins = "http://localhost:3000")
 public class BookController {
 
  @Autowired
--- src/main/java/com/org/mybookstore/api/cart/CartController.java
+++ src/main/java/com/org/mybookstore/api/cart/CartController.java
@@ -3,6 +3,7 @@ package com.org.mybookstore.api.cart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/cart")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CartController {
 
 	@Autowired
@@ -40,7 +42,7 @@ public class CartController {
 			return ResponseEntity.status(HttpStatus.OK).body(cartService.getCartItems(user));
 	}
 
-	@PostMapping("/add")
+	@GetMapping("/add")
 	public ResponseEntity<?> addBookToCart(HttpServletRequest request, @RequestParam Long bookId,
 			@RequestParam int quantity) {
 		String token = request.getHeader("Authorization")!=null?request.getHeader("Authorization").substring(7):"";
